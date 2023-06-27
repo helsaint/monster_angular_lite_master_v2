1e62a0269b4bab2809c96b8efc157c24fe0a9137
helsaint <alexei.ramotar@gmail.com>
2023-06-27 10:25:57 -0400

    Choropleth map added

--- src/app/dashboard/dashboard-components/choropleth/choropleth.component.ts
+++ src/app/dashboard/dashboard-components/choropleth/choropleth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiGeneralService } from '../../../api-general.service';
 
 @Component({
   selector: 'app-choropleth',
@@ -11,20 +12,41 @@ export class ChoroplethComponent implements OnInit {
     data: [{
       type: 'choropleth',
       locationmode: 'ISO-3',
-      locations: [],
-      z: []
+      locations: [] as string[],
+      z: [] as number[],
+      text: "players",
+      colorscale: 'Greens',
+      autocolorscale: false,
+      showscale: true,
     }
     ],
     layout:{
       geo:{
         projection: { type: 'equirectangular' }
-      }
+      },
+      title: {text: 'Map of player nationality'},
+      responsive: true,
+      margin: {
+        l: 50,
+        r: 20,
+        t: 50,
+        b: 50,
+        pad: 0
+      },
     }
   };
 
-  constructor() { }
+  playerData:any=null;
+
+  constructor(private api: ApiGeneralService) { }
 
   ngOnInit(): void {
+    this.api.getChoroplethData().subscribe((data)=>{
+      this.playerData = data;
+      this.graph.data[0].locations = Object.keys(this.playerData);
+      this.graph.data[0].z = Object.values(this.playerData);
+      //this.graph.data[0].text = Object.values(this.playerData);
+    })
   }
 
 }
