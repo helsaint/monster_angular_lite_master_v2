33f5770c2ebac60f8ecfed5a0a5c3f428f73b064
helsaint <alexei.ramotar@gmail.com>
2023-06-27 09:03:08 -0400

    Added link for choropleth data

--- src/app/api-general.service.ts
+++ src/app/api-general.service.ts
@@ -11,11 +11,15 @@ export class ApiGeneralService {
   getPlayers(){
     //return this.http.get('https://restcountries.com/v3.1/all');
     return this.http.get('https://dashboards.aramotar.com/sofifa_api/sofifa_general_stats_api');
-    //return this.http.get('http://127.0.0.1:8000/sofifa_general_api/sofifa_general_stats_api')
+    //return this.http.get('http://127.0.0.1:8000/sofifa_general_api/sofifa_general_stats_api');
   }
 
   getBubblePlotEarnings(){
-    //return this.http.get('http://127.0.0.1:8000/sofifa_general_api/sofifa_bubble_plot_earnings_api')
-    return this.http.get("https://dashboards.aramotar.com/sofifa_api/sofifa_bubble_plot_earnings_api")
+    //return this.http.get('http://127.0.0.1:8000/sofifa_general_api/sofifa_bubble_plot_earnings_api');
+    return this.http.get("https://dashboards.aramotar.com/sofifa_api/sofifa_bubble_plot_earnings_api");
+  }
+
+  getChoroplethData(){
+    return this.http.get("http://127.0.0.1:8000/sofifa_general_api/sofifa_choropleth_api");
   }
 }
